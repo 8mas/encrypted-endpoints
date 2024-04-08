a9b92f3273b85d0256dfbfbc6f6047c2058df9e7
8mas <51927781+8mas@users.noreply.github.com>
2024-04-08 10:43:43 +0000

    docstrings

--- src/fastapi-example/ee/fastapi/middleware.py
+++ src/fastapi-example/ee/fastapi/middleware.py
@@ -32,13 +32,28 @@ class Segment:
     segment_url_type: SegmentURLType = SegmentURLType.NONE
 
 
-# TODO Feature: return segments with URL type
+# TODO Feature: return segments with URL type (for better post decrypt filtering)
 # TODO Feature: token mode: use a unique token per user (statefull)
 # TODO Feature: mac mode: use a mac for every ULR without encryption
 class EncryptedURL:
+    """EncryptedURL. A class to handle encrypted URLs. Provides methods to decrypt and encrypt URLs."""
+
     SHARED_KEY_TYPE = "!"
 
     def __init__(self, path_and_query: str, DELIMITER: str = "~") -> None:
+        """
+        Initializes an object to parse and store segments of a URL, distinguishing between encrypted
+        and cleartext parts using a specified delimiter. This is particularly useful for URLs containing
+        dynamic parts that cannot be encrypted server-side. The segments are stored in an encrypted form and
+        require decryption through a separate call to access their cleartext values.
+
+        Args:
+            path_and_query (str): The URL's path and query string to be parsed. This should include both the
+                path and the query component of the URL, if present.
+            DELIMITER (str, optional): The delimiter used to separate encrypted parts from cleartext parts within
+                the URL. This is crucial for URLs with dynamic parts that are not encrypted
+                server-side. The default delimiter is "~".
+        """
         self.DELIMITER = DELIMITER
         self.path_segments = self._parse_encrypted_url(path_and_query)
 
@@ -83,6 +98,14 @@ class EncryptedURL:
         return segments
 
     def get_full_url(self, shared_segment=True) -> str:
+        """Get the full URL from the stored segments. Non-mutable operation.
+
+        Args:
+            shared_segment (bool, optional): Whether to include the shared version or not.
+
+        Returns:
+            str: The full URL
+        """
         url = ""
         for segment in self.path_segments:
             if segment.segment_type == SegmentType.ENCRYPTED:
@@ -99,7 +122,15 @@ class EncryptedURL:
             and self.path_segments[-1].segment_type == SegmentType.SHARED_KEY
         )
 
-    def decrypt_url(self, main_key: bytes, identifier: bytes):
+    def decrypt_url(self, main_key: bytes, identifier: bytes) -> list[Segment]:
+        """
+        Decrypts the encrypted segments of the URL stored within the object, using the specified main key
+        and identifier. This operation is non-mutable.
+
+        Returns:
+            list[Segment]: A list of Segment objects, each representing a decrypted segment of the URL. The
+                Segment class encapsulates both the decrypted content and the segment type.
+        """
         if not self.path_segments:
             return []
 
@@ -120,9 +151,14 @@ class EncryptedURL:
             elif segment.segment_type == SegmentType.PLAIN:
                 decrypted_segments.append(segment)
 
-        return decrypted_segments  # todo really segements or just one string?
+        return decrypted_segments
 
     def get_shareable_url(self, main_key: bytes, identifier: bytes) -> str:
+        """Creates a shareable version of the currently stored URL. Non-mutable operation.
+
+        Returns:
+            str: The new shareable URL
+        """
         if not self.path_segments:
             return ""
         if self.is_shared_url():
@@ -146,6 +182,16 @@ class EncryptedURL:
     def encrypt_value(
         main_key: bytes, value: bytes, identifier: bytes, delimiter="~"
     ) -> str:
+        """Encrypts a value with the main key and identifier.
+        This function is indidirectly called by the middleware.
+
+        Args:
+            delimiter (str, optional): A string used to delimit encrypted segments within the resultant encrypted
+                value. Defaults to "~", but can be customized based on requirements.
+
+        Returns:
+            str: The encrypted representation of the value, including delimiters as specified.
+        """
         derived_key = EncryptedURL._derive_key(main_key, identifier)
         encryptor = EncryptedURL._get_encryptor(derived_key)
         encrypted_value = encryptor.encrypt(value, None)
@@ -164,9 +210,40 @@ class EncryptedURL:
         return kdf.derive(main_key + identifier)
 
 
-# TODO Feature support for link sharing & session ressumption
-# TODO docstrings
+# TODO Feature: support for link sharing & session ressumption
 class EncryptedEndpointsMiddleware:
+    """EncryptedEndpointsMiddleware. Intercepts requests and decrypts the URL path and query parameters.
+    Passes the decrypted URL back to the application.
+
+        Args:
+        app: The FastAPI application instance that the middleware is attached to. This is the application
+             that will receive the decrypted URLs for handling.
+        main_key (bytes): The key used to derive client-specific keys for encryption and decryption.
+
+        templates (Optional, default=None): An optional Jinja2Templates instance to which the encryption
+            function can be added. This allows for dynamic encryption of URLs within server-rendered templates.
+            e.g.: templates from 'templates = Jinja2Templates(directory="templates")'
+
+        encryption_function_name (str, optional): The name of the encryption function to be made available
+            within Jinja templates for encrypting URL components. Defaults to "encrypt_value".
+
+        identifier_extractor (Callable[[Request], bytes], optional): A function that extracts a client-specific
+            identifier from the incoming request, such as an IP address or a session token.
+            Defaults to extracting the client's IP address.
+
+        pre_decrypt_filter_route (Callable[[str], bool], optional): A function to determine whether certain routes
+            should bypass the decryption process. This is useful for paths that should remain unencrypted, such as
+            static file routes or health check endpoints.
+
+        url_validator (Callable[[list[Segment]], bool], optional): A function that validates the decrypted URL against
+            certain criteria to ensure its integrity and validity. This can prevent malformed or maliciously crafted URLs
+            from being processed by the application. Should be implemented when partial url encryption is supported.
+            Then users could guess and use valid endpoints.
+
+        delimiter (str, optional): The delimiter used to separate encrypted segments within the URL. This should be a
+            character or sequence of characters that does not typically appear in URLs, to avoid conflicts. Defaults to "~".
+    """
+
     def __init__(
         self,
         app,
@@ -174,7 +251,8 @@ class EncryptedEndpointsMiddleware:
         templates=None,
         encryption_function_name="encrypt_value",
         identifier_extractor: Callable[[Request], bytes] = None,
-        filter_route: Callable[[str], bool] = None,
+        pre_decrypt_filter_route: Callable[[str], bool] = None,
+        url_validator: Callable[[list[Segment]], bool] = None,
         delimiter="~",
     ):
         self.app = app
@@ -184,17 +262,20 @@ class EncryptedEndpointsMiddleware:
             identifier_extractor
             or EncryptedEndpointsMiddleware.default_extract_identifier
         )
-        self.filter_route = filter_route if filter_route else lambda x: False
+        self.pre_decrypt_filter_route = (
+            pre_decrypt_filter_route if pre_decrypt_filter_route else lambda x: False
+        )
+        self.url_validator = url_validator if url_validator else lambda x: False
 
         if templates:
             templates.env.globals[encryption_function_name] = self.encrypt_value
 
     async def __call__(self, scope, receive, send):
-        if scope["type"] != "http":
-            return await self.app(scope, receive, send)
-        if scope["path"] == "/favicon.ico":
-            return await self.app(scope, receive, send)
-        if self.filter_route(scope["path"]):
+        if (
+            scope["type"] != "http"
+            or scope["path"] == "/favicon.ico"
+            or self.pre_decrypt_filter_route(scope["path"])
+        ):
             return await self.app(scope, receive, send)
 
         new_scope = scope.copy()
@@ -214,7 +295,9 @@ class EncryptedEndpointsMiddleware:
             # Enforce that the path starts with a slash
             if url_segments and not url_segments[0].value.startswith("/"):
                 url_segments[0].value = "/" + url_segments[0].value
-            # todo! check here if path and query are in allowed route
+
+            if self.url_validator(url_segments):
+                return await self.app(new_scope, receive, send)
 
             path_and_query = "".join([segment.value for segment in url_segments])
             decrypted_url = urlparse(path_and_query)
@@ -229,6 +312,12 @@ class EncryptedEndpointsMiddleware:
         return await self.app(new_scope, receive, send)
 
     def encrypt_value(self, value: bytes, request: Request) -> str:
+        """Encrypts given value with the identifier of the request.
+        This function is called from the Jinja2 template when specified.
+
+        Returns:
+            str: The encrypted value
+        """
         if type(value) == int:
             value = str(value).encode()
         if type(value) == str:
